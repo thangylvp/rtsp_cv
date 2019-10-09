ae8e892a58a4d5469773fa13e6d9076cbd1dc0f4
thangylvp <thang.yenlacvinhphuc.98@gmail.com>
2019-10-09 18:00:57 +0700

    modify to opencv 3, add multithread

--- src/1.GetStream/main.cpp
+++ src/1.GetStream/main.cpp
@@ -1,8 +1,79 @@
 #include <iostream>
-#include <opencv4/opencv2/opencv.hpp>
+#include <cstdlib>
+#include <pthread.h>
+#include <opencv2/opencv.hpp>
+using namespace std;
 
-int main(){
-    cv::VideoCapture cap("rtsp://admin:zaq1XSW2@192.168.1.64:554/ch1/main/av_stream", cv::CAP_FFMPEG);
+#define NUM_THREADS 4
+
+struct thread_data {
+    int  thread_id;
+    string *message;
+};
+
+void *PrintHello(void *threadarg) {
+    struct thread_data *my_data;
+    my_data = (struct thread_data *) threadarg;
+
+    // cout << "Thread ID : " << my_data->thread_id  << " Message : " << my_data->message << endl;
+    // cv::VideoCapture cap(my_data->message, cv::CAP_FFMPEG);
+    // if (!cap.isOpened())
+    // {   
+    //     std::cout << "Input error\n";
+    //     pthread_exit(NULL);
+    // }
+    
+    cout << "Thread ID : " << my_data->thread_id  << " Message : " << *(my_data->message) << endl;
+    cv::VideoCapture cap(*(my_data->message));
+    if (!cap.isOpened())
+    {   
+        std::cout << "Input error\n";
+        pthread_exit(NULL);
+    }
+
+    cv::Mat frame;
+    int cnt = 0;
+    string name;
+    while(1) {
+        cap >> frame;
+        name = std::to_string(my_data->thread_id) + "___" + std::to_string(cnt) + ".jpg";
+        cnt++;
+        cv::imwrite(name, frame);
+        
+    }
+    cap.release();
+
+    pthread_exit(NULL);
+}
+
+int main () {
+    pthread_t threads[NUM_THREADS];
+    struct thread_data td[NUM_THREADS];
+    int rc;
+    int i;
+
+    for( i = 0; i < NUM_THREADS; i++ ) {
+        cout <<"main() : creating thread, " << i << endl;
+        td[i].thread_id = i;
+        string tmp = "rtsp://admin:123@Abc.@10.10.0." + std::to_string(i + 164) + ":554/Streaming/Channels/101/";
+        td[i].message = new string(tmp);
+        rc = pthread_create(&threads[i], NULL, PrintHello, (void *)&td[i]);
+        
+        if (rc) {
+            cout << "Error:unable to create thread," << rc << endl;
+            exit(-1);
+        }
+        
+    }
+
+    for( i = 0; i < NUM_THREADS; i++ ) {
+        pthread_join(threads[i], NULL);
+    }
+    pthread_exit(NULL);
+}
+
+/*
+    cv::VideoCapture cap("rtsp://admin:123@Abc.@10.10.0.166:554/Streaming/Channels/101/", cv::CAP_FFMPEG);
     if (!cap.isOpened())
     {   
         std::cout << "Input error\n";
@@ -19,4 +90,5 @@ int main(){
         }
     }
     cap.release();
-}
+    */
+
